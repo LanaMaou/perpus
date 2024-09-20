e0ae721ae82b6af7076478664516f47405d5a1e5
4dydd <haidiraditya16@gmail.com>
2024-09-20 20:31:12 +0800

    before menambahkan komponen Sebuku

---
+++ resources/js/Components/Sebuku.jsx
@@ -0,0 +1,16 @@
+function Sebuku({ key, value }) {
+    return (
+        <>
+            {/* <ul
+                key={key}
+                className="w-1/2 h-32 px-5 py-2 mb-3 text-blue-100 bg-blue-700 rounded-md text-start bg-opacity-90"
+            >
+                <li>{value.judul}</li>
+                <li>{value.penulis}</li>
+                <li>{value.tahun_terbit}</li>
+            </ul> */}
+        </>
+    );
+}
+
+export default Sebuku;
